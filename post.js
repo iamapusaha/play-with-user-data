13b76841636a374fc8ff22836f35ccc7f337f8d0
iamapusaha <apusaha392@gmail.com>
2023-08-28 12:28:32 +0600

    loaded data

--- post.js
+++ post.js
@@ -1,1 +1,6 @@
-console.log("connected!");
+function loadUser() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+loadUser();
